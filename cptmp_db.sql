703595bd88eee8e448d1b1c18b745c0df4952e43
GuoHaoLi <2215228145@qq.com>
2020-07-09 20:05:06 +0800

    :memo:
    更新数据库

--- cptmp_db.sql
+++ cptmp_db.sql
@@ -1,137 +1,247 @@
-create table cptmp_user
-(
-    id                      bigint unsigned auto_increment
-        primary key,
-    gmt_create              datetime         not null,
-    gmt_modified            datetime         null,
-    idx_password            varchar(128)     not null,
-    uk_username             varchar(20)      not null,
-    introduction            text             null,
-    phone_number            decimal(11)      null,
-    gender                  tinyint unsigned null,
-    avatar                  varchar(200)     null,
-    idx_role_name           varchar(25)      not null,
-    enabled                 tinyint          not null,
-    account_non_expired     tinyint          not null,
-    credentials_non_expired tinyint          not null,
-    account_non_locked      tinyint          not null,
-    invitation_code         varchar(6)       null,
-    uk_email                varchar(200)     not null,
-    constraint invitation_code_UNIQUE
-        unique (invitation_code),
-    constraint uk_email
-        unique (uk_email)
-);
+CREATE DATABASE  IF NOT EXISTS `cptmp_db` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `cptmp_db`;
+-- MySQL dump 10.13  Distrib 8.0.19, for Win64 (x86_64)
+--
+-- Host: localhost    Database: cptmp_db
+-- ------------------------------------------------------
+-- Server version	8.0.19
 
-create table activity_record
-(
-    id           bigint unsigned auto_increment
-        primary key,
-    gmt_create   datetime        not null,
-    gmt_modified datetime        null,
-    idx_user_id  bigint unsigned not null,
-    idx_team_id  bigint unsigned not null,
-    state_record int unsigned    not null,
-    event_record varchar(2000)   not null,
-    constraint activity_user_id
-        foreign key (idx_user_id) references cptmp_user (id)
-);
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
-create index activity_user_id_idx
-    on activity_record (idx_user_id);
+--
+-- Table structure for table `activity_record`
+--
 
-create table daily_record
-(
-    id            bigint unsigned auto_increment
-        primary key,
-    gmt_create    datetime        not null,
-    gmt_modified  datetime        null,
-    idx_user_id   bigint unsigned not null,
-    idx_team_id   bigint unsigned not null,
-    document_path varchar(200)    not null,
-    record_type   int unsigned    not null,
-    title         varchar(100)    not null,
-    content       text            not null,
-    constraint daily_user_id
-        foreign key (idx_user_id) references cptmp_user (id)
-);
+DROP TABLE IF EXISTS `activity_record`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `activity_record` (
+  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
+  `gmt_create` datetime NOT NULL,
+  `gmt_modified` datetime DEFAULT NULL,
+  `idx_user_id` bigint unsigned NOT NULL,
+  `idx_team_id` bigint unsigned NOT NULL,
+  `state_record` int unsigned NOT NULL,
+  `event_record` varchar(2000) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `activity_user_id_idx` (`idx_user_id`),
+  CONSTRAINT `activity_user_id` FOREIGN KEY (`idx_user_id`) REFERENCES `cptmp_user` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-create index daily_user_id_idx
-    on daily_record (idx_user_id);
+--
+-- Table structure for table `cptmp_user`
+--
 
-create table enterprise_admin
-(
-    id             bigint unsigned auto_increment
-        primary key,
-    gmt_create     datetime        not null,
-    gmt_modified   datetime        null,
-    idx_name       varchar(20)     not null,
-    uk_user_id     bigint unsigned not null,
-    uk_employee_id bigint          not null,
-    constraint uk_employee_id
-        unique (uk_employee_id),
-    constraint uk_user_id
-        unique (uk_user_id),
-    constraint enterprise_user_id
-        foreign key (uk_user_id) references cptmp_user (id)
-);
+DROP TABLE IF EXISTS `cptmp_user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `cptmp_user` (
+  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
+  `gmt_create` datetime NOT NULL,
+  `gmt_modified` datetime DEFAULT NULL,
+  `idx_password` varchar(128) NOT NULL,
+  `uk_username` varchar(200) NOT NULL,
+  `nickname` varchar(200) NOT NULL,
+  `introduction` text,
+  `uk_email` varchar(200) NOT NULL,
+  `phone_number` decimal(11,0) DEFAULT NULL,
+  `gender` tinyint unsigned DEFAULT NULL,
+  `avatar` varchar(200) DEFAULT NULL,
+  `idx_role_name` varchar(25) NOT NULL,
+  `enabled` tinyint NOT NULL,
+  `account_non_expired` tinyint NOT NULL,
+  `credentials_non_expired` tinyint NOT NULL,
+  `account_non_locked` tinyint NOT NULL,
+  `invitation_code` varchar(6) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `uk_email_UNIQUE` (`uk_email`),
+  UNIQUE KEY `uk_username_UNIQUE` (`uk_username`),
+  UNIQUE KEY `invitation_code_UNIQUE` (`invitation_code`)
+) ENGINE=InnoDB AUTO_INCREMENT=22 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-create table password_reset_token
-(
-    id           bigint unsigned auto_increment
-        primary key,
-    gmt_create   datetime     not null,
-    gmt_modified datetime     null,
-    token        varchar(128) not null,
-    idx_email    varchar(200) not null,
-    constraint token
-        unique (token),
-    constraint password_reset_email
-        foreign key (idx_email) references cptmp_user (uk_email)
-);
+--
+-- Table structure for table `daily_record`
+--
 
-create table school_instructor
-(
-    id              bigint unsigned auto_increment
-        primary key,
-    gmt_create      datetime        not null,
-    gmt_modified    datetime        null,
-    idx_name        varchar(20)     not null,
-    uk_user_id      bigint unsigned not null,
-    uk_employee_id  bigint          null,
-    idx_school_name varchar(20)     not null,
-    constraint uk_employee_id
-        unique (uk_employee_id),
-    constraint uk_user_id
-        unique (uk_user_id),
-    constraint teacher_user_id
-        foreign key (uk_user_id) references cptmp_user (id)
-);
+DROP TABLE IF EXISTS `daily_record`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `daily_record` (
+  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
+  `gmt_create` datetime NOT NULL,
+  `gmt_modified` datetime DEFAULT NULL,
+  `idx_user_id` bigint unsigned NOT NULL,
+  `idx_team_id` bigint unsigned NOT NULL,
+  `document_path` varchar(200) NOT NULL,
+  `record_type` int unsigned NOT NULL,
+  `title` varchar(100) NOT NULL,
+  `content` text NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `daily_user_id_idx` (`idx_user_id`),
+  CONSTRAINT `daily_user_id` FOREIGN KEY (`idx_user_id`) REFERENCES `cptmp_user` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-create table school_student
-(
-    id              bigint unsigned auto_increment
-        primary key,
-    gmt_create      datetime        not null,
-    gmt_modified    datetime        null,
-    idx_name        varchar(20)     not null,
-    idx_school_name varchar(20)     not null,
-    uk_user_id      bigint unsigned not null,
-    uk_student_id   bigint          null,
-    uk_student_face varchar(200)    null,
-    constraint uk_student_id
-        unique (uk_student_id),
-    constraint uk_user_id
-        unique (uk_user_id),
-    constraint stu_user_id
-        foreign key (uk_user_id) references cptmp_user (id)
-);
+--
+-- Table structure for table `enterprise_admin`
+--
 
-create table test_tb
-(
-    id          int unsigned auto_increment
-        primary key,
-    pigeon_name varchar(20) not null
-)
-    charset = utf8;
+DROP TABLE IF EXISTS `enterprise_admin`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `enterprise_admin` (
+  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
+  `gmt_create` datetime NOT NULL,
+  `gmt_modified` datetime DEFAULT NULL,
+  `idx_name` varchar(20) NOT NULL,
+  `uk_user_id` bigint unsigned NOT NULL,
+  `uk_employee_id` bigint NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `uk_employee_id` (`uk_employee_id`),
+  UNIQUE KEY `uk_user_id` (`uk_user_id`),
+  CONSTRAINT `enterprise_user_id` FOREIGN KEY (`uk_user_id`) REFERENCES `cptmp_user` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Table structure for table `password_reset_token`
+--
+
+DROP TABLE IF EXISTS `password_reset_token`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `password_reset_token` (
+  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
+  `gmt_create` datetime NOT NULL,
+  `gmt_modified` datetime DEFAULT NULL,
+  `token` varchar(128) NOT NULL,
+  `idx_email` varchar(200) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `token` (`token`),
+  KEY `password_reset_email` (`idx_email`),
+  CONSTRAINT `password_reset_email` FOREIGN KEY (`idx_email`) REFERENCES `cptmp_user` (`uk_email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `school_instructor`
+--
+
+DROP TABLE IF EXISTS `school_instructor`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `school_instructor` (
+  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
+  `gmt_create` datetime NOT NULL,
+  `gmt_modified` datetime DEFAULT NULL,
+  `idx_name` varchar(20) NOT NULL,
+  `uk_user_id` bigint unsigned NOT NULL,
+  `uk_employee_id` bigint DEFAULT NULL,
+  `idx_school_name` varchar(20) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `uk_user_id` (`uk_user_id`),
+  UNIQUE KEY `uk_employee_id` (`uk_employee_id`),
+  CONSTRAINT `teacher_user_id` FOREIGN KEY (`uk_user_id`) REFERENCES `cptmp_user` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `school_student`
+--
+
+DROP TABLE IF EXISTS `school_student`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `school_student` (
+  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
+  `gmt_create` datetime NOT NULL,
+  `gmt_modified` datetime DEFAULT NULL,
+  `idx_name` varchar(20) NOT NULL,
+  `idx_school_name` varchar(20) NOT NULL,
+  `uk_user_id` bigint unsigned NOT NULL,
+  `uk_student_id` bigint DEFAULT NULL,
+  `uk_student_face` varchar(200) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `uk_user_id` (`uk_user_id`),
+  UNIQUE KEY `uk_student_id` (`uk_student_id`),
+  CONSTRAINT `stu_user_id` FOREIGN KEY (`uk_user_id`) REFERENCES `cptmp_user` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `test_tb`
+--
+
+DROP TABLE IF EXISTS `test_tb`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `test_tb` (
+  `id` int unsigned NOT NULL AUTO_INCREMENT,
+  `pigeon_name` varchar(20) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `train_project`
+--
+
+DROP TABLE IF EXISTS `train_project`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `train_project` (
+  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
+  `gmt_create` datetime NOT NULL,
+  `gmt_modified` datetime DEFAULT NULL,
+  `uk_project_name` varchar(100) NOT NULL,
+  `start_date` datetime DEFAULT NULL,
+  `finish_date` datetime DEFAULT NULL,
+  `content` text NOT NULL,
+  `accept_standard` text NOT NULL,
+  `resource_library` text NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `uk_project_name` (`uk_project_name`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `train_team`
+--
+
+DROP TABLE IF EXISTS `train_team`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `train_team` (
+  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
+  `gmt_create` datetime NOT NULL,
+  `gmt_modified` datetime DEFAULT NULL,
+  `train_project_id` bigint unsigned NOT NULL,
+  `idx_team_name` varchar(100) NOT NULL,
+  `team_manager_id` bigint unsigned NOT NULL,
+  `code_base_url` text NOT NULL,
+  `team_grade` decimal(3,2) unsigned DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2020-07-09 20:03:21
